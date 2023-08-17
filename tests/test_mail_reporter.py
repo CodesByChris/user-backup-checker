02a2a9d6822503f09d38798f064744250193cdaf
Christian Zingg <czingg@ethz.ch>
2023-08-17 10:25:27 +0200

    Added test plan.

--- tests/test_mail_reporter.py
+++ tests/test_mail_reporter.py
@@ -13,3 +13,28 @@ def test_future_recipients(reporter):
     names = {u.username for u in mail_reporter.future_recipients}
     assert names == {"future_1", "future_2"}
 
+
+# - Test future_recipients():
+#     - >1 persons,
+#     - no future recipients but others,
+#     - no users
+# - Test outdated_recipients():
+#     - 2 persons & one gets mail, the other not
+#     - 2 persons & both get mail
+#     - 2 persons & none gets mail
+#     - no outdated recipients but others,
+#     - no users
+# - notify_outdated_recipients():
+#     - >1 persons,
+#     - no users
+#     - Compare produced mails
+#     - no users --> test that nothing is sent
+# - notify_future_recipients():
+#     - >1 persons,
+#     - no users
+#     - Compare produced mail
+#     - no users --> test that nothing is sent
+
+
+# - Unify notify_future* and notify_outdated* into combined test functions!
+#     - Doesn't work for all tests of the getters! Different logic!
