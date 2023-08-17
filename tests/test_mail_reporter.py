81d6d167ffcff8efc22c9077ea4b1f9f8557aa58
Christian Zingg <czingg@ethz.ch>
2023-08-17 11:42:57 +0200

    Added test_no_outdated_recipients.

--- tests/test_mail_reporter.py
+++ tests/test_mail_reporter.py
@@ -5,13 +5,12 @@ from unittest.mock import MagicMock
 from ubc.user_backup_checker import CONFIG, MailClient, MailReporter, StatusReporter
 
 
+# Test MailReporter.future_recipients
 def test_future_recipients(reporter):
     """Tests MailReporter.future_recipients."""
     mail_reporter = MailReporter(reporter, MagicMock(), timedelta(5))
-    assert len(mail_reporter.future_recipients) == 2
-
-    names = {u.username for u in mail_reporter.future_recipients}
-    assert names == {"future_1", "future_2"}
+    assert mail_reporter.future_recipients == reporter.future_users
+    # Note: alphabetic ordering is implicitly also tested (lists).
 
 
 def test_no_future_recipients(mock_reporter_args):
@@ -19,9 +18,19 @@ def test_no_future_recipients(mock_reporter_args):
     mock_reporter_args["users"] = [u for u in mock_reporter_args["users"]
                                    if u.username not in {"future_1", "future_2"}]
     mail_reporter = MailReporter(StatusReporter(**mock_reporter_args), MagicMock(), timedelta(5))
-    assert len(mail_reporter.future_recipients) == 0
+    assert not mail_reporter.future_recipients
 
 
+# Test MailReporter.outdated_recipients
+def test_no_outdated_recipients(mock_reporter_args):
+    """Tests outdated_recipients when no such users exist whereas others do."""
+    mock_reporter_args["users"] = [u for u in mock_reporter_args["users"]
+                                   if u.username not in {"outdated_1", "outdated_2"}]
+    mail_reporter = MailReporter(StatusReporter(**mock_reporter_args), MagicMock(), timedelta(5))
+    assert not mail_reporter.outdated_recipients
+
+
+# Test no users
 def test_no_users(mock_reporter_args):
     """Tests MailReporter for an empty user list."""
     mock_reporter_args["users"] = []
@@ -29,8 +38,8 @@ def test_no_users(mock_reporter_args):
     mail_reporter = MailReporter(StatusReporter(**mock_reporter_args), mail_mock, timedelta(5))
 
     # Test status detection
-    assert len(mail_reporter.future_recipients) == 0
-    assert len(mail_reporter.outdated_recipients) == 0
+    assert not mail_reporter.future_recipients
+    assert not mail_reporter.outdated_recipients
 
     # Test that no mails are sent
     mail_reporter.notify_future_recipients(CONFIG["SUBJECT_FUTURE"], CONFIG["MAIL_FUTURE"])
@@ -43,7 +52,6 @@ def test_no_users(mock_reporter_args):
 #     - 2 persons & one gets mail, the other not
 #     - 2 persons & both get mail
 #     - 2 persons & none gets mail
-#     - no outdated recipients but others,
 # - notify_outdated_recipients():
 #     - >1 persons,
 #     - no users
